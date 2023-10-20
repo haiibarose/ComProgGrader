f5d1fc2b181f2b840fb1ce15ef97df707d6122a5
haiibarose <ruk.rossukhon@gmail.com>
2023-10-20 13:35:51 +0700

    add 01_Expr_03.py

--- 01_Expr_03.py
+++ 01_Expr_03.py
@@ -6,6 +6,6 @@ up_3 = math.log(9.7, math.e)
 power = 7/math.sqrt(71) - math.sin(math.radians(40))
 
 up = up_1 - up_2 + up_3**power
-down =(1.2)**(2.3**(1/3))
+down = (1.2)**(2.3**(1/3))
 ans = up/down
 print(round(ans, 6))
