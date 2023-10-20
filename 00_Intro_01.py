0644f6e2d9d855779d8fad51488cba3d8de7c027
haiibarose <ruk.rossukhon@gmail.com>
2023-10-20 12:59:56 +0700

    00_Intro_01.py

---
+++ 00_Intro_01.py
@@ -0,0 +1,15 @@
+print("""Hello Python.\nWe're using Python 3.""")
+
+# or
+
+# print('''Hello Python.\nWe're using Python 3.''')
+
+# or
+
+# print("Hello Python.")
+# print("We're using Python 3.")
+
+# or
+
+# print('Hello Python.')
+# print('We're using Python 3.')
