f55141aeab531e9532b7aca132bae1fb4c2a3817
haiibarose <ruk.rossukhon@gmail.com>
2023-10-20 13:34:47 +0700

    add 01_Expr_03.py

---
+++ 01_Expr_03.py
@@ -0,0 +1,11 @@
+import math
+
+up_1 = math.pi
+up_2 = math.factorial(10)/8**8
+up_3 = math.log(9.7, math.e)
+power = 7/math.sqrt(71) - math.sin(math.radians(40))
+
+up = up_1 - up_2 + up_3**power
+down =(1.2)**(2.3**(1/3))
+ans = up/down
+print(round(ans, 6))
