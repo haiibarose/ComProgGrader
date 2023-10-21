41d409174c34307df7ec2c60c9033f2fb966803c
haiibarose <ruk.rossukhon@gmail.com>
2023-10-21 21:44:09 +0700

    add 05_List_12.py

---
+++ 05_List_12.py
@@ -0,0 +1,46 @@
+n = int(input())
+
+for i in range(n):
+    name = input()
+    if (name == 'Robert'):
+        print('Dick')
+    elif (name == 'Dick'):
+        print('Robert')
+    elif (name == 'William'):
+        print('Bill')
+    elif (name == 'Bill'):
+        print('William')
+    elif (name == 'James'):
+        print('Jim')
+    elif (name == 'Jim'):
+        print('James')
+    elif (name == 'John'):
+        print('Jack')
+    elif (name == 'Jack'):
+        print('John')
+    elif (name == 'Margaret'):
+        print('Peggy')
+    elif (name == 'Peggy'):
+        print('Margaret')
+    elif (name == 'Edward'):
+        print('Ed')
+    elif (name == 'Ed'):
+        print('Edward')
+    elif (name == 'Sarah'):
+        print('Sally')
+    elif (name == 'Sally'):
+        print('Sarah')
+    elif (name == 'Andrew'):
+        print('Andy')
+    elif (name == 'Andy'):
+        print('Andrew')
+    elif (name == 'Anthony'):
+        print('Tony')
+    elif (name == 'Tony'):
+        print('Anthony')
+    elif (name == 'Deborah'):
+        print('Debbie')
+    elif (name == 'Debbie'):
+        print('Deborah')
+    else:
+        print('Not found')
